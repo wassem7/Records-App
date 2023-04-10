025656e7a1acb3360bc87e3ab344af723a230f6b
Wassem Darkwa <waseemdarkwa@gmail.com>
2023-04-10 10:02:08 +0100

    minor changes

--- app/Searchbar.tsx
+++ app/Searchbar.tsx
@@ -12,7 +12,7 @@ const Searchbar = () => {
       <div className='flex bg-slate-600 px-2 py-3 md:py-4 rounded-2xl flex-1 '>
         <input
           type='text'
-          placeholder='Search with  Name ..'
+          placeholder='Search with Name ..'
           className='outline-none flex-1 placeholder-slate-400  bg-slate-600 text-slate-300 mx-2'
         />
         <div className='mx-2'>
